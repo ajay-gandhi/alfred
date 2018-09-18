ec85ba7c4cc950ae305680fc19eafd258fa47f76
Ajay Gandhi <aag255@cornell.edu>
2018-09-18 11:03:33 -0700

    Move everything into functions

--- app.js
+++ app.js
@@ -4,111 +4,149 @@ const creds = require("./creds");
 
 const URLS = {
   login: "https://www.seamless.com/corporate/login/",
-  choose_rest: "https://www.seamless.com/UpdateMemberMealsStep2.m?vendorLocationId=46193&browseBy=OpenRestaurants&clickPage=DEFAULT&clickPageLocation=DEFAULT&clickRank=14&precedencePolicyId=",
+  choose_rest: "https://www.seamless.com/meals.m",
 };
 
 const TIME = "10:00 PM";
 const RESTAURANT = "Bamboo".toLowerCase();
-const DISH = "Shioyaki".toLowerCase();
 const ORDERS = {
   "Shioyaki": [],
   "soda": ["sprite"],
 };
 const NAMES = [["Johan", "Augustine"], ["James", "Wei"]];
 
+const DRY_RUN = true;
+
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    defaultViewport: {
+      width: 1200,
+      height: 900,
+    },
   });
   const page = await browser.newPage();
+
   try {
-    await page.goto(URLS.login);
-
-    await page.click("input#username");
-    await page.keyboard.type(creds.username);
-
-    await page.click("input#password");
-    await page.keyboard.type(creds.password);
-
-    await page.click("a#submitLogin");
-    await page.waitForNavigation();
+    await login_to_seamless(page, creds);
 
     // Should be logged in now
+    await order_from_restaurant(page, RESTAURANT, ORDERS, NAMES);
 
-    /* Choose time */
-    await page.select("#time", TIME).catch(() => {});
-    await page.click("tr.startorder a");
-    await page.waitForNavigation();
-    /* End */
-
-    // Open the page for restaurant we want
-
-    /* Choose restaurant */
-    const rest_links = await page.$$("a[name=\"vendorLocation\"]");
-    let our_rest;
-    for (const anchor of rest_links) {
-      const text = await page.evaluate(e => e.innerText.toLowerCase(), anchor);
-      if (text.includes(RESTAURANT)) our_rest = anchor;
+    if (!DRY_RUN) {
+      await browser.close();
     }
-    await our_rest.click();
-    await page.waitForNavigation();
-    /* End */
-
-    // Add to order
-
-    /* Choose items */
-    const orders = Object.entries(ORDERS);
-    for (const [item, options] of orders) {
-      // Click menu item
-      const item_links = await page.$$("a[name=\"product\"]");
-      let our_item;
-      for (const anchor of item_links) {
-        const text = await page.evaluate(e => e.innerText.toLowerCase(), anchor);
-        if (text.includes(item.toLowerCase())) our_item = anchor;
-      }
-      await our_item.click();
-      await page.waitFor(1000);
-
-      // Select options
-      const option_links = await page.$$("li label");
-      for (const input of option_links) {
-        const text = await page.evaluate(e => e.innerText.toLowerCase(), input);
-        const is_selected = options.reduce((memo, o) => memo || text.includes(o.toLowerCase()), false);
-        if (is_selected) await input.click();
-      }
-
-      // Click add to order
-      await page.click("a#a1");
-      await page.waitFor(2000);
-    }
-
-    await page.click("a.findfoodbutton");
-    await page.waitForNavigation();
-    /* End */
-
-    /* Add user */
-    for (const name of NAMES) {
-      await page.click("p#RecentAllocations a");
-      await page.click("input#FirstName");
-      await page.keyboard.type(name[0]);
-
-      await page.click("input#LastName");
-      await page.keyboard.type(name[1]);
-
-      await page.click("tr#AddUser h4.PrimaryLink a");
-      await page.waitForNavigation();
-    }
-
-    await page.click("td.delete a");
-    await page.waitForNavigation();
-    /* End */
-
-    // Order!
-    // await page.click("a.findfoodbutton");
-    // await page.waitForNavigation();
-
-    // await browser.close();
   } catch (err) {
     console.log("Crashed with error", err);
   }
 })();
+
+/**
+ * Given a puppeteer page, logs into Seamless with the given credentials.
+ */
+const login_to_seamless = async (page, creds) => {
+  await page.goto(URLS.login);
+
+  await page.click("input#username");
+  await page.keyboard.type(creds.username);
+
+  await page.click("input#password");
+  await page.keyboard.type(creds.password);
+
+  await page.click("a#submitLogin");
+  await page.waitForNavigation();
+};
+
+/**
+ * Given a page with a logged-in status, this function will submit an order
+ * at the given restaurant with the given items for the given names.
+ */
+const order_from_restaurant = async (page, restaurant, orders, names) => {
+  await page.goto(URLS.choose_rest);
+
+  await page.select("#time", TIME).catch(() => {});
+  await page.click("tr.startorder a");
+  await page.waitForNavigation();
+
+  // Choose restaurant
+  const rest_links = await page.$$("a[name=\"vendorLocation\"]");
+  let our_rest;
+  for (const anchor of rest_links) {
+    const text = await page.evaluate(e => e.innerText.toLowerCase(), anchor);
+    if (text.includes(RESTAURANT)) our_rest = anchor;
+  }
+  await our_rest.click();
+  await page.waitForNavigation();
+
+  // Do the rest!
+  await fill_orders(page, orders);
+  await fill_names(page, names);
+
+  // Submit order
+  if (!DRY_RUN) {
+    await page.click("a.findfoodbutton");
+    await page.waitForNavigation();
+  }
+};
+
+/**
+ * Given a page at the order stage, this function will add the given orders to
+ * the cart.
+ *
+ * The orders parameter should be an object of this form:
+ *   {
+ *     "dish":  ["option 1", "option 2"],
+ *     "dish2": [], // No options
+ *   }
+ */
+const fill_orders = async (page, orders) => {
+  const orders_as_arrays = Object.entries(orders);
+  for (const [item, options] of orders_as_arrays) {
+    // Click menu item
+    const item_links = await page.$$("a[name=\"product\"]");
+    let our_item;
+    for (const anchor of item_links) {
+      const text = await page.evaluate(e => e.innerText.toLowerCase(), anchor);
+      if (text.includes(item.toLowerCase())) our_item = anchor;
+    }
+    await our_item.click();
+    await page.waitFor(1000);
+
+    // Select options
+    const option_links = await page.$$("li label");
+    for (const input of option_links) {
+      const text = await page.evaluate(e => e.innerText.toLowerCase(), input);
+      const is_selected = options.reduce((memo, o) => memo || text.includes(o.toLowerCase()), false);
+      if (is_selected) await input.click();
+    }
+
+    // Click add to order
+    await page.click("a#a1");
+    await page.waitFor(2000);
+  }
+
+  await page.click("a.findfoodbutton");
+  await page.waitForNavigation();
+};
+
+/**
+ * Given a page at the checkout stage, this function will enter the given names.
+ * The names parameter should be an array of tuples, where each tuple contains
+ * the first and last names of all those involved in the order.
+ */
+const fill_names = async (page, names) => {
+  for (const name of names) {
+    await page.click("p#RecentAllocations a");
+    await page.click("input#FirstName");
+    await page.keyboard.type(name[0]);
+
+    await page.click("input#LastName");
+    await page.keyboard.type(name[1]);
+
+    await page.click("tr#AddUser h4.PrimaryLink a");
+    await page.waitForNavigation();
+  }
+
+  await page.click("td.delete a");
+  await page.waitForNavigation();
+};
