8938a7c562908330c5776bd5d1fb9956ecfe0681
Ajay Gandhi <aag255@cornell.edu>
2018-09-18 17:22:07 -0700

    Add more to integration test

--- test/integration.js
+++ test/integration.js
@@ -5,6 +5,6 @@ const perform_order = require("../perform_ordering");
 console.log(record_order.add_order("bamboo", [["shioyaki", []]], "Johan Augustine"));
 console.log(record_order.add_order("bamboo", [["soda", ["sprite"]], ["tempura combo", []]], "James Wei"));
 
-// console.log(record_order.add_order("extreme pizza", [["cheese pizza (medium)", []]], "Jay Jung"));
+console.log(record_order.add_order("extreme pizza", [["cheese pizza (medium)", []]], "Jay Jung"));
 
 setTimeout(perform_order, 3000);
