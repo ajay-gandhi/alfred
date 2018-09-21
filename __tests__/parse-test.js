b5546a3e0c0adb5729ed1dd56cd4db65cd0e23a5
Ajay Gandhi <aag255@cornell.edu>
2018-09-21 13:11:11 -0700

    Fix tests to reflect new format (without passing name, etc)
    Also add test for parsing user

--- __tests__/parse-test.js
+++ __tests__/parse-test.js
@@ -3,19 +3,18 @@
 const { parse_command } = require("../parse");
 
 // Try adding orders
-const test_name = "Ajay Gandhi";
 const test_inputs = [
   "alfred order chicken momo from newa",
   "alfred order chicken momo, chicken momo [large] from newa",
   "alfred order aquafina, cheese pizza (indee) [green peppers, chicken] from extreme pizza",
   "alfred order chicken momo from newa at 5pm",
   "alfred forget",
+  "alfred info ajay gandhi, 1234567890",
 ];
 
 const test_outputs = [
   {
-    command: "add_order",
-    name: test_name,
+    command: "order",
     params: {
       restaurant: "newa",
       items: [
@@ -24,8 +23,7 @@ const test_outputs = [
     },
   },
   {
-    command: "add_order",
-    name: test_name,
+    command: "order",
     params: {
       restaurant: "newa",
       items: [
@@ -35,8 +33,7 @@ const test_outputs = [
     },
   },
   {
-    command: "add_order",
-    name: test_name,
+    command: "order",
     params: {
       restaurant: "extreme pizza",
       items: [
@@ -46,8 +43,7 @@ const test_outputs = [
     },
   },
   {
-    command: "add_order",
-    name: test_name,
+    command: "order",
     params: {
       time: "5pm",
       restaurant: "newa",
@@ -57,29 +53,39 @@ const test_outputs = [
     },
   },
   {
-    command: "remove_order",
-    name: test_name,
+    command: "forget",
     params: {},
+  },
+  {
+    command: "info",
+    params: {
+      name: "ajay gandhi",
+      phone: "1234567890",
+    },
   },
 ];
 
 test("parses basic add order", () => {
-  expect(parse_command(test_name, test_inputs[0])).toEqual(test_outputs[0]);
+  expect(parse_command(test_inputs[0])).toEqual(test_outputs[0]);
 });
 
 test("parses add order with one option", () => {
-  expect(parse_command(test_name, test_inputs[1])).toEqual(test_outputs[1]);
+  expect(parse_command(test_inputs[1])).toEqual(test_outputs[1]);
 });
 
 test("parses add order with multiple options", () => {
-  expect(parse_command(test_name, test_inputs[2])).toEqual(test_outputs[2]);
+  expect(parse_command(test_inputs[2])).toEqual(test_outputs[2]);
 });
 
 test("parses add order with a time", () => {
-  expect(parse_command(test_name, test_inputs[2])).toEqual(test_outputs[2]);
+  expect(parse_command(test_inputs[3])).toEqual(test_outputs[3]);
 });
 
 test("parses remove order", () => {
-  expect(parse_command(test_name, test_inputs[3])).toEqual(test_outputs[3]);
+  expect(parse_command(test_inputs[4])).toEqual(test_outputs[4]);
+});
+
+test("parses adding user", () => {
+  expect(parse_command(test_inputs[5])).toEqual(test_outputs[5]);
 });
 
