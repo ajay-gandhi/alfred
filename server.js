d011e7118fb913b14db8b54003f82d89741e670d
Ajay Gandhi <aag255@cornell.edu>
2018-09-26 18:53:12 -0700

    Switch to Koa since fastify https doesn't work, support https (only)!

--- server.js
+++ server.js
@@ -1,63 +1,78 @@
 
-const fastify = require("fastify")({ logger: true });
+const https = require("https");
+const fs = require("fs");
+const app = new (require("koa"))();
+const router = new (require("koa-router"))();
+
+const Logger = require("../util/logger");
 
 const { parse_command } = require("./parse");
 const RecordOrders = require("./record_orders");
 const Users = require("./users");
 const { slackIncomingToken } = require("./creds");
 
+const LOG = new Logger("alfred");
 const PORT = process.argv[2] || 9002;
 
-fastify.register(require("fastify-formbody"));
-fastify.register(require("fastify-static"), {
-  root: `${__dirname}/confirmations`,
-  prefix: "/confirmations/",
+app.use(require("koa-bodyparser")());
+app.use(require("koa-static")(`${__dirname}/public`));
+
+app.use((ctx, next) => {
+  LOG.log(`${ctx.method} ${ctx.url}`);
+  next();
 });
 
-fastify.route({
-  method: "POST",
-  url: "/command",
-  handler: async (req, res) => {
-    if (req.body.token !== slackIncomingToken) {
-      console.log("Request does not have proper secret");
-      return {};
-    }
-    if (!req.body.text || !req.body.user_name) {
-      console.log("Request is missing username or text");
-      return {};
-    }
-
-    const username = req.body.user_name;
-    const parsed = parse_command(req.body.text);
-
-    switch (parsed.command) {
-      case "order":
-        const order = RecordOrders.add_order(parsed.params.restaurant, parsed.params.items, username);
-        const items_list = order.items.map(i => i[0]).join(", ");
-        return { text: `Added ${items_list} from ${order.restaurant}` };
-        break;
-
-      case "forget":
-        const order = RecordOrders.remove_order(username);
-        return { text: `Removed order from ${order.restaurant}` };
-        break;
-
-      case "info":
-        Users.add_user(username, parsed.params.name, parsed.params.phone);
-        return { text: `Added information for ${username}` };
-        break;
-
-      default:
-        return {};
-    }
-  },
-});
-
-(async () => {
-  try {
-    await fastify.listen(PORT);
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
+router.post("/command", (ctx, next) => {
+  if (ctx.request.body.token !== slackIncomingToken) {
+    LOG.log("Request does not have proper secret");
+    return {};
   }
-})();
+  if (!ctx.request.body.text || !ctx.request.body.user_name) {
+    LOG.log("Request is missing username or text");
+    return {};
+  }
+
+  const username = ctx.request.body.user_name;
+  const parsed = parse_command(ctx.request.body.text);
+
+  switch (parsed.command) {
+    case "order": {
+      const order = RecordOrders.add_order(parsed.params.restaurant, parsed.params.items, username);
+      const items_list = order.items.map(i => i[0]).join(", ");
+      ctx.body = { text: `Added ${items_list} from ${order.restaurant}` };
+      break;
+    }
+
+    case "forget": {
+      const order = RecordOrders.remove_order(username);
+      ctx.body = { text: `Removed order from ${order.restaurant}` };
+      break;
+    }
+
+    case "info": {
+      Users.add_user(username, parsed.params.name, parsed.params.phone);
+      ctx.body = { text: `Added information for ${username}` };
+      break;
+    }
+
+    default:
+      ctx.body = "{}";
+  }
+  next();
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use((ctx) => {
+  if (ctx.body) LOG.log(`Responded with "${ctx.body.text}"`);
+  ctx.body = JSON.stringify(ctx.body);
+});
+
+const httpsOpts = {
+  key: fs.readFileSync(`${__dirname}/privkey.pem`).toString().trim(),
+  cert: fs.readFileSync(`${__dirname}/fullchain.pem`).toString().trim(),
+};
+https.createServer(httpsOpts, app.callback()).listen(PORT, () => {
+  LOG.log(`Server listening on port ${PORT}`);
+});
