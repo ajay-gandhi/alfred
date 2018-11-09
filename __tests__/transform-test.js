1dcb8a3dcb38b6d4a3827e454c51b3dbda3c5c49
Ajay Gandhi <aag255@cornell.edu>
2018-11-08 16:47:41 -0800

    Move menu functions into transform util, more tests

--- __tests__/transform-test.js
+++ __tests__/transform-test.js
@@ -1,59 +1,99 @@
-// Tests for parser.js
+// Tests for util/transform.js
 
 const Transform = require("../util/transform");
 
-// Try adding orders
-const testInputs = [
-  {},
-  {
-    "alice": {
-      "restaurant": "american",
-      "items": [["hot dog", []], ["burger", ["lettuce"]]],
-    },
-    "bob": {
-      "restaurant": "italian",
-      "items": [["pizza", ["onions"]], ["pasta", []]],
-    },
-    "cad": {
-      "restaurant": "italian",
-      "items": [["pizza", ["chicken"]]],
-    },
-  }
-];
-
-const testOutputs = [
-  [],
-  [
+describe("indexByRestaurantAndUser", () => {
+  // Try adding orders
+  const testInputs = [
+    {},
     {
-      "restaurant": "american",
-      "users": [
-        {
-          "username": "alice",
-          "items": [["hot dog", []], ["burger", ["lettuce"]]],
-        },
-      ],
-    },
-    {
-      "restaurant": "italian",
-      "users": [
-        {
-          "username": "bob",
-          "items": [["pizza", ["onions"]], ["pasta", []]],
-        },
-        {
-          "username": "cad",
-          "items": [["pizza", ["chicken"]]],
-        },
-      ],
-    },
-  ],
-];
+      "alice": {
+        "restaurant": "american",
+        "items": [["hot dog", []], ["burger", ["lettuce"]]],
+      },
+      "bob": {
+        "restaurant": "italian",
+        "items": [["pizza", ["onions"]], ["pasta", []]],
+      },
+      "cad": {
+        "restaurant": "italian",
+        "items": [["pizza", ["chicken"]]],
+      },
+    }
+  ];
 
-test("transforms empty data", () => {
-  expect(Transform.indexByRestaurantAndUser(testInputs[0])).toEqual(testOutputs[0]);
+  const testOutputs = [
+    [],
+    [
+      {
+        "restaurant": "american",
+        "users": [
+          {
+            "username": "alice",
+            "items": [["hot dog", []], ["burger", ["lettuce"]]],
+          },
+        ],
+      },
+      {
+        "restaurant": "italian",
+        "users": [
+          {
+            "username": "bob",
+            "items": [["pizza", ["onions"]], ["pasta", []]],
+          },
+          {
+            "username": "cad",
+            "items": [["pizza", ["chicken"]]],
+          },
+        ],
+      },
+    ],
+  ];
+
+  test("transforms empty data", () => {
+    expect(Transform.indexByRestaurantAndUser(testInputs[0])).toEqual(testOutputs[0]);
+  });
+
+  test("transforms several data", () => {
+    expect(Transform.indexByRestaurantAndUser(testInputs[1])).toEqual(testOutputs[1]);
+  });
 });
 
-test("transforms several data", () => {
-  expect(Transform.indexByRestaurantAndUser(testInputs[1])).toEqual(testOutputs[1]);
+describe("correctRestaurant", () => {
+  test("corrects a valid restaurant", () => {
+    expect(Transform.correctRestaurant("newa").name).toEqual("Newa (Ellis St)");
+  });
+
+  test("returns false for an invalid restaurant", () => {
+    const invalidRest = "nwea";
+    expect(Transform.correctRestaurant(invalidRest).error).toEqual(`Couldn't find restaurant called "${invalidRest}"`);
+  });
 });
 
+describe("parseOrders", () => {
+  test("basic order parse", () => {
+    expect(Transform.parseOrders("chicken momo and lassi", "Newa (Ellis St)")).toEqual({
+      "correctedItems": [
+        ["Chicken Momo", []],
+        ["Mango Lassi", []],
+      ],
+    });
+  });
+
+  test("order with options", () => {
+    expect(Transform.parseOrders("chicken momo (large)", "Newa (Ellis St)")).toEqual({
+      "correctedItems": [
+        ["Chicken Momo", ["large"]],
+      ],
+    });
+  });
+
+  test("multiple items with options", () => {
+    expect(Transform.parseOrders("cheese pizza indee (mushrooms, chicken), boneless wings (5 lb)", "Extreme Pizza (Folsom)")).toEqual({
+      "correctedItems": [
+        ["Classic Cheese Pizza (Indee)", ["mushrooms", "chicken"]],
+        ["Boneless Wings", ["5 lb"]],
+      ],
+    });
+  });
+});
--- util/transform.js
+++ util/transform.js
@@ -1,6 +1,15 @@
 /**
  * Module for transforming data
  *
+ * This file contains functions that take in data and return the same data in a
+ * different format. Some functions may use other external tools, such as menu
+ * data.
+ */
+
+const FuzzAldrin = require("fuzzaldrin");
+const MenuData = require("../data/menu_data");
+
+/**
  * Orders data is stored persistently in a JSON hash keyed by username, with the
  * value being a hash containing the restaurant and items. This allows for easy
  * overwriting and removing of orders.
@@ -30,4 +39,94 @@ module.exports.indexByRestaurantAndUser = (data) => {
   }
 
   return Object.values(newData);
+};
+
+/**
+ * Uses menu data to convert an inputted restaurant to the correct name
+ */
+const restaurantNames = Object.keys(MenuData);
+module.exports.correctRestaurant = (restInput) => {
+  // Convert restaurant to official name
+  const matches = FuzzAldrin.filter(restaurantNames, restInput);
+  if (matches.length === 0) {
+    return { error: `Couldn't find restaurant called "${restInput}"` };
+  } else {
+    return MenuData[matches[0]];
+  }
+};
+
+/**
+ * Given a string containing a list of orders and a restaurant, parses out the
+ * items and corrects them
+ */
+module.exports.parseOrders = (input, r) => {
+  const parts = [""];
+  let parenCount = 0;
+  const delimiters = [", and", ",and", " and ", ", ", ","];
+  for (let i = 0; i < input.length; i++) {
+    // Don't want to split within parentheses
+    if (input[i] === "(") {
+      parenCount++;
+    } else if (input[i] === ")") {
+      parenCount--;
+    } else {
+      for (let j = 0; j < delimiters.length; j++) {
+        if (input.substring(i, i + delimiters[j].length) === delimiters[j] && parenCount === 0) {
+          parts.push("");
+          i += delimiters[j].length;
+          break;
+        }
+      }
+    }
+    parts[parts.length - 1] += input[i];
+  }
+
+  return transformOrders(parts.reduce((m, e) => e ? m.concat(e.trim()) : m, []), r);
+};
+
+/********************************** Helpers ***********************************/
+
+// Remove leading "X. "
+const friendlizeItem = i => i.replace(/^[\w]{1,3}\. /, "");
+
+// Use menu data to correct item
+const findCorrectItem = (restaurantName, itemName) => {
+  const menu = MenuData[restaurantName].menu;
+  const matches = FuzzAldrin.filter(menu, itemName, { key: "name" });
+  return matches.length === 0 ? false : matches[0];
+};
+
+// Parse out options
+const OPTIONS_REGEX = /\((.*)\)/;
+const transformOrders = (items, restaurantName) => {
+  const errors = [];
+  const correctedItems = items.map((item) => {
+    // First parse out options
+    const matchedOptions = item.match(OPTIONS_REGEX);
+    const formattedItem = [];
+    if (matchedOptions) {
+      const options = matchedOptions[1].split(",").map(o => o.trim());
+      formattedItem.push(item.slice(0, item.indexOf("(")).trim());
+      formattedItem.push(options);
+    } else {
+      formattedItem.push(item);
+      formattedItem.push([]);
+    }
+
+    // Correct item name
+    const correctedItem = findCorrectItem(restaurantName, formattedItem[0]);
+    if (correctedItem) {
+      formattedItem[0] = correctedItem.name;
+    } else {
+      errors.push(formattedItem[0]);
+    }
+    return formattedItem;
+  });
+
+  if (errors.length > 0) {
+    const itemNoun = errors.length === 1 ? "item" : "items";
+    const items = errors.map(i => `"${i}"`).join(", ");
+    return { error: `Couldn't find ${itemNoun} called ${items}` };
+  }
+  return { correctedItems };
 };
