073a35d12d5280f9b7f34cfac59391fff728b5cd
Ajay Gandhi <aag255@cornell.edu>
2019-02-04 16:59:03 -0800

    Convert stats to mongo

--- stats.js
+++ stats.js
@@ -2,10 +2,15 @@
  * Module for interacting with persistent stats data
  */
 
-const fs = require("fs");
+const MongoClient = require("mongodb").MongoClient;
+const private = require("./private");
 
-const STATS_FILE = `${__dirname}/data/stats.json`;
-const stats = fs.existsSync(STATS_FILE) ? JSON.parse(fs.readFileSync(STATS_FILE)) : {};
+let stats;
+const client = new MongoClient(private.mongoSrv, { useNewUrlParser: true });
+client.connect((err) => {
+  if (err) console.log("Error connecting to MongoDB:", err);
+  stats = client.db(private.mongoDbName).collection("stats");
+});
 
 const N_TOP_DISHES = 3;
 
@@ -14,160 +19,204 @@ const N_TOP_DISHES = 3;
 /**
  * Return how many times the given user has been called
  */
-const getCallsForUser = u => stats[u] && stats[u].calls || 0;
+const getCallsForUser = async (username) => {
+  const s = await stats.findOne({ username });
+  return s ? s.calls : 0;
+};
 
 /**
  * Return the dollar value that the user spent at this restaurant
  */
-const getDollarsForRestaurant = (u, r) => stats[u] && stats[u][r] && stats[u][r] && stats[u][r].dollars || 0;
+const getDollarsForRestaurant = async (username, restaurantName) => {
+  return (await stats.findOne({ username })).restaurants[restaurantName].dollars;
+};
 
 /**
  * Return the dollar value that the user spent
  */
-const getDollarsForUser = u => stats[u] && Object.keys(stats[u]).reduce((m, r) => m + getDollarsForRestaurant(u, r), 0) || 0;
+const getDollarsForUser = async (username) => {
+  const s = await stats.findOne({ username });
+  return Object.keys(s.restaurants).reduce((m, r) => m + s.restaurants[r].dollars, 0);
+};
 
 /**
  * Return the dollar value that all users spent
  */
-const getTotalDollars = () => Object.keys(stats).reduce((m, u) => m + getDollarsForUser(u), 0);
+const getTotalDollars = async () => {
+  const allStats = await stats.find({}).toArray();
+  return allStats.reduce((total, s) => {
+    return total + Object.keys(s.restaurants).reduce((m, r) => m + s.restaurants[r].dollars, 0);
+  }, 0);
+};
 
 /**
  * Return the top dishes for this user at this restaurant
  */
-const getTopDishesForRestaurant = (user, restaurant, givenTops) => {
-  if (!stats[user] || !stats[user][restaurant]) return [];
+const getTopDishesForRestaurant = async (username, restaurant) => {
+  const s = fromMongo((await stats.findOne({ username })).restaurants[restaurant].items);
+  return Object.keys(s).reduce((tops, itemName) => {
+    if (tops.length < 3) {
+      return tops.concat({
+        itemName,
+        count: s[itemName],
+      });
+    }
 
-  const tops = givenTops || [];
-
-  Object.keys(stats[user][restaurant].items).forEach((itemName) => {
     for (let i = 0; i < N_TOP_DISHES; i++) {
-      if (!tops[i] || stats[user][restaurant].items[itemName] > tops[i].count) {
+      if (s[itemName] > tops[i].count) {
         tops.splice(i, 0, {
-          restaurant,
           itemName,
-          count: stats[user][restaurant].items[itemName],
+          count: s[itemName],
         });
-        if (givenTops) tops[i].restaurant = restaurant;
-        if (tops.length > N_TOP_DISHES) tops.pop();
+        tops.pop();
         break;
       }
     }
-  });
-  return tops.filter(t => t.count > 0);
+    return tops;
+  }, []);
 };
 
 /**
  * Return the top dishes for this user
  */
-const getTopDishesForUser = (user) => {
-  if (!stats[user]) return [];
+const getTopDishesForUser = async (username) => {
+  const s = fromMongo((await stats.findOne({ username })).restaurants);
 
-  return Object.keys(stats[user]).reduce((memo, restaurant) => {
-    // Call this tech debt LOL I'm lazy
-    return restaurant === "calls" ? memo : getTopDishesForRestaurant(user, restaurant, memo);
-  }, []).filter(t => t.count > 0);
+  return Object.keys(s).reduce((userTops, restaurant) => {
+    return Object.keys(s[restaurant].items).reduce((restTops, itemName) => {
+      if (restTops.length < 3) {
+        return restTops.concat({
+          itemName,
+          count: s[restaurant].items[itemName],
+          restaurant,
+        });
+      }
+
+      for (let i = 0; i < N_TOP_DISHES; i++) {
+        if (s[restaurant].items[itemName] > restTops[i].count) {
+          restTops.splice(i, 0, {
+            itemName,
+            count: s[restaurant].items[itemName],
+            restaurant,
+          });
+          restTops.pop();
+          break;
+        }
+      }
+      return restTops;
+    }, userTops);
+  }, []);
 };
 
 /**
  * Return the top dishes for all users
  */
-const getTopDishes = () => {
-  // Consolidate data (remove user abstraction)
-  const consolidated = {};
-  Object.keys(stats).forEach((user) => {
-    Object.keys(stats[user]).forEach((restaurant) => {
-      if (restaurant === "calls") return;
-      if (!consolidated[restaurant]) consolidated[restaurant] = {};
-      Object.keys(stats[user][restaurant].items).forEach((itemName) => {
-        if (consolidated[restaurant][itemName]) {
-          consolidated[restaurant][itemName] += stats[user][restaurant].items[itemName];
-        } else {
-          consolidated[restaurant][itemName] = stats[user][restaurant].items[itemName];
-        }
-      });
+const getTopDishes = async () => {
+  const allStats = fromMongo(await stats.find({}).toArray());
+
+  // First create array of all restaurant data (removes user association)
+  const restaurantData = allStats.reduce((memo, userStats) => {
+    const restDataAsList = Object.keys(userStats.restaurants).map((restaurantName) => {
+      return {
+        name: restaurantName,
+        items: userStats.restaurants[restaurantName].items,
+      };
     });
-  });
+    return memo.concat(restDataAsList);
+  }, []);
 
-  const tops = [];
-  for (let i = 0; i < N_TOP_DISHES; i++) { tops.push({ count: 0 }); }
+  // Consolidate restaurant data into single object
+  const crd = restaurantData.reduce((memo, data) => {
+    if (!memo[data.name]) memo[data.name] = {};
 
-  Object.keys(consolidated).forEach((restaurant) => {
-    Object.keys(consolidated[restaurant]).forEach((itemName) => {
+    Object.keys(data.items).forEach((itemName) => {
+      if (!memo[data.name][itemName]) memo[data.name][itemName] = 0;
+      memo[data.name][itemName] += data.items[itemName];
+    });
+    return memo;
+  }, {});
+
+  // Given consolidated restaurant data find tops
+  // Basically same logic as getTopDishesForUser
+  return Object.keys(crd).reduce((allTops, restaurant) => {
+    return Object.keys(crd[restaurant]).reduce((restTops, itemName) => {
+      if (restTops.length < 3) {
+        return restTops.concat({
+          itemName,
+          count: crd[restaurant][itemName],
+          restaurant,
+        });
+      }
+
       for (let i = 0; i < N_TOP_DISHES; i++) {
-        if (consolidated[restaurant][itemName] > tops[i].count) {
-          tops.splice(i, 0, {
-            restaurant,
+        if (crd[restaurant][itemName] > restTops[i].count) {
+          restTops.splice(i, 0, {
             itemName,
-            count: consolidated[restaurant][itemName],
+            count: crd[restaurant][itemName],
+            restaurant,
           });
-          tops.pop();
+          restTops.pop();
           break;
         }
       }
-    });
-  });
-
-  return tops.filter(t => !!t.restaurant);
+      return restTops;
+    }, allTops);
+  }, []);
 };
 
 /********************************** Exports ***********************************/
 
 // The following functions consolidate data from above helpers
-const getStatsForUserFromRestaurant = (user, restaurant) => ({
-  dollars: getDollarsForRestaurant(user, restaurant),
-  dishes: getTopDishesForRestaurant(user, restaurant),
+const getStatsForUserFromRestaurant = async (user, restaurant) => ({
+  dollars: await getDollarsForRestaurant(user, restaurant),
+  dishes: await getTopDishesForRestaurant(user, restaurant),
 });
-const getStatsForUser = (user) => ({
-  calls: getCallsForUser(user),
-  dollars: getDollarsForUser(user),
-  dishes: getTopDishesForUser(user),
+const getStatsForUser = async (user) => ({
+  calls: await getCallsForUser(user),
+  dollars: await getDollarsForUser(user),
+  dishes: await getTopDishesForUser(user),
 });
-const getGlobalStats = () => ({
-  dollars: getTotalDollars(),
-  dishes: getTopDishes(),
+const getGlobalStats = async () => ({
+  dollars: await getTotalDollars(),
+  dishes: await getTopDishes(),
 });
 
 /**
- * Record that the given user ordered the given dollar amount from the given
- * restaurant
+ * Record the given set of stats
  */
-const recordDollars = (user, restaurant, dollars) => {
-  if (!stats[user])                     stats[user] = {};
-  if (!stats[user][restaurant])         stats[user][restaurant] = {};
-  if (!stats[user][restaurant].dollars) stats[user][restaurant].dollars = 0;
-  stats[user][restaurant].dollars += dollars;
-};
+const recordStats = async (username, restaurant, dollars, items, calls) => {
+  const changes = items.reduce((memo, [itemName]) => {
+    const key = `restaurants.${restaurant}.items.${toMongo(itemName)}`;
+    memo[key] = memo[key] ? memo[key] + 1 : 1;
+    return memo;
+  }, {});
 
-/**
- * Record that the given user ordered the given item from the given restaurant
- */
-const recordDish = (user, restaurant, itemName) => {
-  if (!stats[user])                             stats[user] = {};
-  if (!stats[user][restaurant])                 stats[user][restaurant] = {};
-  if (!stats[user][restaurant].items)           stats[user][restaurant].items = {};
-  if (!stats[user][restaurant].items[itemName]) stats[user][restaurant].items[itemName] = 0;
-  stats[user][restaurant].items[itemName]++;
-};
+  changes[`restaurants.${restaurant}.dollars`] = dollars;
+  if (calls) changes["calls"] = 1;
 
-/**
- * Record that the given user was selected for a call
- */
-const recordCall = (user) => {
-  if (!stats[user]) stats[user] = {};
-  stats[user].calls = stats[user].calls ? stats[user].calls + 1 : 1;
+  await stats.findOneAndUpdate({ username }, { $inc: changes }, { upsert: true });
 };
-
-/**
- * Save the new data to disk
- */
-const save = () => fs.writeFileSync(STATS_FILE, JSON.stringify(stats, null, 2));
 
 module.exports = {
   getStatsForUserFromRestaurant,
   getStatsForUser,
   getGlobalStats,
-  recordDollars,
-  recordDish,
-  recordCall,
-  save,
+  recordStats,
+};
+
+/********************************* Sanitizers *********************************/
+
+// These functions sanitize data so that it can safely be passed to Mongo
+const toMongo = s => s.replace(".", "%PD%");
+const fromMongo = (o) => {
+  if (typeof o !== "object") return o;
+
+  if (o instanceof Array) {
+    return o.map(fromMongo);
+  } else {
+    return Object.keys(o).reduce((memo, key) => {
+      memo[key.replace("%PD%", ".")] = fromMongo(o[key]);
+      return memo;
+    }, {});
+  }
 };
