32d96f6579e1a16ef9113cf31c3449f5a4ba03b2
Ajay Gandhi <aag255@cornell.edu>
2019-04-08 11:50:11 -0700

    New add restaurant script

---
+++ util/add_restaurant.js
@@ -0,0 +1,24 @@
+/**
+ * Adds a restaurant to be scraped
+ *
+ * Basically just adds an empty entry to Mongo so that the next time the scraper
+ * runs it'll pick up and scrape.
+ */
+
+const MongoClient = require("mongodb").MongoClient;
+const priv = require("../private");
+
+let menu;
+const client = new MongoClient(priv.mongoSrv, { useNewUrlParser: true });
+client.connect((err) => {
+  if (err) console.log("Error connecting to MongoDB:", err);
+  client
+    .db(priv.mongoDbName)
+    .collection("menu")
+    .insertOne({ name: process.argv[2] })
+    .then(() => {
+      console.log(`Added ${process.argv[2]} to scraper!`);
+      process.exit(0);
+    });
+});
+
