4a1c7f20bcab1aafedfa557db073e213e1e7a526
Ajay Gandhi <aag255@cornell.edu>
2019-04-12 13:51:14 -0700

    Write proper logger

---
+++ logger.js
@@ -0,0 +1,38 @@
+const { createLogger, format, transports } = require("winston");
+
+const logPrinter = format.printf(({ level, message, stack, file, timestamp }) => {
+  let msg = message;
+  if (message instanceof Error) {
+    msg = message.stack;
+  } else if (typeof message === "object") {
+    msg = JSON.stringify(message, null, 2);
+  }
+  const lvl = level === "error" ? "ERROR" : level;
+  return `${timestamp} [${file}] ${lvl}: ${msg}`;
+});
+const testing = format(m => { console.log(m); return m });
+
+const logger = createLogger({
+  level: "info",
+  format: format.combine(
+    format.timestamp({
+      format: "YYYY-MM-DD HH:mm:ss"
+    }),
+    logPrinter,
+  ),
+  defaultMeta: { service: "alfred" },
+});
+
+if (process.stdin.isTTY) {
+  // Log to console if dev
+  logger.add(new transports.Console());
+} else {
+  const prefix = `${__dirname}/../../logs/`;
+  logger.add(new transports.File({ filename: `${prefix}alfred-error.log`, level: "error" }));
+  logger.add(new transports.File({ filename: `${prefix}alfred-combined.log` }));
+}
+
+module.exports = (file) => ({
+  info:  m => logger.info(m, { file }),
+  error: m => logger.error(m, { file }),
+});
